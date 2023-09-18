73c615971720b6cc3d67eb495e9a0c895f2b7aff
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2023-09-18 07:58:06 +0530

    Initial commit

---
+++ application/controllers/Aboutus.php
@@ -0,0 +1,9 @@
+<?php
+class Aboutus extends CI_Controller {
+
+    public function index() {
+        // Load the view for your static page
+        $this->load->view('aboutus');
+    }
+}
+?>
---
+++ application/views/aboutus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+        <link rel="stylesheet" href="<?= base_url('assets/css/aboutus.css') ?>">
+    </head>
+    <?php $this->load->view('common_components/header'); ?>
+    <body>
+
+
+ 
+        <section id="content">
+            <section class="commonbanner">
+                <div class="sub_banner_overlay"></div>
+                <img src="https://www.nolimitcars.co.uk/wp/wp-content/uploads/2020/02/about-us.jpg">
+            </section>
+            <section class="subpagecontent">
+                <img src="https://www.nolimitcars.co.uk/wp/wp-content/uploads/2020/02/faq-content.jpg">
+                <div class="wrapper">
+                    <div class="subcontent_wrapper">
+                        <h1>About us  </h1>
+                        <p>NoLimit Cars offer a highly professional taxi, minibus, chauffeur, parcel and executive service who are airport and distance specialists. We have been serving customers for over 15 years, now new management, new price.</p>
+                        <p>When you require a taxi we can meet your needs 24/7.</p>
+                        <p>Our Airport Transfer service is a speciality but we will also undertake work going to or from almost anywhere and willing to do any distance, any time!</p>
+                        <p>We operate a wide range of vehicles capable of comfortably seating between 4 and 16 passengers. We pride ourselves in providing a high quality service and our rates are highly competitive.</p>
+                        <p>All our taxis are licensed and badged and vehicles undergo a regular safety check which is carried out by the relevant Council.</p>
+                        <p>Bookings can be made online, by email or by telephone although email should only be used for advance bookings. NoLimit Cars will confirm such bookings by return e-mail.</p>
+                        <p>NoLimit Cars also provide account facilities, and are available for regular school runs, these can be set-up upon request. We accept all card payments.</p>
+                        <p>Remember online, Airport, Distance and Local Specialists will meet your needs and always put customers first.</p>
+                        
+                        <div class="subbottom_text">If you would like a quote for a journey you are planning please use our online booking form or call us on&nbsp;<a href="tel:02039822911">02039 822 911</a>.</div>
+                    </div>
+                </div>
+            </section>
+        </section>
+    </body>
+    </html>
