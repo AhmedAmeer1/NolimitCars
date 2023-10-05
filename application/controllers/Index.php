ec6a6ee64043d97e372940d157025784c92f6536
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2023-10-05 07:35:47 +0530

    email test

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -385,16 +385,16 @@ public function check_promo_code(){
 		$config['bcc_batch_mode'] = FALSE; // TRUE or FALSE (boolean)    Enable BCC Batch Mode.
 		$config['bcc_batch_size'] = 200; // Number of emails in each BCC batch.
 		$this->email->initialize($config);
-		$this->email->from('bookings@nolimitcars.co.uk', 'NOLIMIT CARS TEST');
+		$this->email->from('bookings@nolimitcars.co.uk', 'NOLIMIT CARS Ahmed');
 		$this->email->to($data['email']);
         $this->email->subject('Your Nolimit Taxi order has been received!');
 		$mesg = $this->load->view('template/email',$data,true);
 		$this->email->message($mesg);
 		if($this->email->send()){
-			$to = "bookings@nolimitcars.co.uk,soumen.karmakar@solutions2xl.com";
+			$to = "ahmedameer1999421@gmail.com,ahmedameerdev@gmail.com";
 			//$to = "soumen.karmakar@solutions2xl.com";
 		$this->email->initialize($config);
-		$this->email->from('bookings@nolimitcars.co.uk', 'NOLIMIT CARS TEST');
+		$this->email->from('bookings@nolimitcars.co.uk', 'NOLIMIT CARS Ahmed');
 		$this->email->to($to);
         $this->email->subject('New Nolimit Taxi order has been received!');
 		$mesg = $this->load->view('template/email_admin',$data,true);
