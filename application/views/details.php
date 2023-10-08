d87887fa35d4eb93dc529a2902bc9778c0ddb35e
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2023-10-08 12:03:50 +0530

    booking pages modal issue

--- application/views/details.php
+++ application/views/details.php
@@ -58,6 +58,7 @@ input[type=number] {
 	}
 
    </style>
+       <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
    <body>
    <?php $this->load->view('common_components/header'); ?>
       <!-- responsive menu -->
@@ -459,7 +460,7 @@ input[type=number] {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
       <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
       <link rel="stylesheet" href="/resources/demos/style.css">
-       <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+   
                <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
                <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
                <script src="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js"></script>
