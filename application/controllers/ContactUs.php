ca067ddfb077d689a9c273f5103655400f30b01e
AhmedAmeer1 <ahmedameer1999421@gmail.com>
2023-10-23 06:44:59 +0530

    cookies testing for contact us

--- application/controllers/ContactUs.php
+++ application/controllers/ContactUs.php
@@ -3,6 +3,7 @@ class ContactUs extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->library('form_validation');
+        $this->load->helper('cookie');
     }
 
     public function index() {
@@ -17,17 +18,21 @@ class ContactUs extends CI_Controller {
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
         $this->form_validation->set_rules('message', 'Message', 'required');
 
+
+
+
         if ($this->form_validation->run() === FALSE) {
             $this->load->view('contactUs');
         } else {
             $name = $this->input->post('name');
             $email = $this->input->post('email');
-
-
-
             $message = $this->input->post('message');
             $contactnumber = $this->input->post('contactnumber');
        
+
+	//Setting cookie
+    set_cookie('username',$name,86400); //86400 seconds is one day for this cookie
+	$username = get_cookie('username');
 
 //----------------------------------------email sending start ----------------------------------------
 
@@ -52,7 +57,7 @@ class ContactUs extends CI_Controller {
 
             $this->load->model('Contact_model');
             if ($this->Contact_model->save_contact($name, $email, $message, $contactnumber)) {
-                $this->session->set_flashdata('success_message', 'Your message has been sent successfully.');
+                $this->session->set_flashdata('success_message', 'Your message has been sent successfully.',$username);
                 redirect(base_url('ContactUs'));
             } else {
                 $this->session->set_flashdata('success_message', 'Your message has been sent successfully.');
